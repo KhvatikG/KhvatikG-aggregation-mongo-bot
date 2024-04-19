8256b654b4a9effa9107b4c3e774ca03fbb4b1de
Garik <khvatik.igor@gmail.com>
2024-04-19 14:19:21 +0300

    install loguru, add aggregate_sum_from_date(work with blanks, does not display dates with a zero)

--- main.py
+++ main.py
@@ -1,20 +1,96 @@
 import asyncio
+from datetime import datetime
+from pprint import pprint
+from typing import Literal
 
 from motor.motor_asyncio import AsyncIOMotorClient, AsyncIOMotorDatabase
-
-from pprint import pprint
+from loguru import logger
 
 uri = "mongodb://localhost:27017/?authSource=admin"
 client = AsyncIOMotorClient(uri)
 db = client.get_database("sampleDB")
 
 
-async def hello_mongo(db_: AsyncIOMotorDatabase):
-    sample_collection = db_.get_collection("sample_collection")
+async def aggregate_sum_from_date(
+        db_: AsyncIOMotorDatabase,
+        dt_from: str,
+        dt_upto: str,
+        group_type: Literal['hour', 'day', 'month']):
 
-    doc = await sample_collection.find_one({"value": 800})
+    if not (group_type == 'hour' or group_type == 'day' or group_type == 'month'):
+        e = f"group_type value is {group_type} but this value should be only ('hour' | 'day' | 'month')"
+        logger.warning(e)
+        return e
 
-    pprint((doc))
+    collection = db_.get_collection("sample_collection")
+
+    date_format = None
+    date_group = None
+
+    if group_type == "month":
+        date_format = "%Y-%m"
+        date_group = {
+            "year": {"$year": "$dt"},
+            "month": {"$month": "$dt"},
+            "day": 1
+        }
+    elif group_type == "day":
+        date_format = "%Y-%m-%d"
+        date_group = {
+            "year": {"$year": "$dt"},
+            "month": {"$month": "$dt"},
+            "day": {"$dayOfMonth": "$dt"}
+        }
+    elif group_type == "hour":
+        date_format = "%Y-%m-%dT%H:00:00"
+        date_group = {
+            "year": {"$year": "$dt"},
+            "month": {"$month": "$dt"},
+            "day": {"$dayOfMonth": "$dt"},
+            "hour": {"$hour": "$dt"}
+        }
+
+    query = [
+        {"$match": {
+            "dt": {
+                "$gte": datetime.fromisoformat(dt_from),
+                "$lte": datetime.fromisoformat(dt_upto)
+            }
+        }},
+        {"$project": {
+            "value": 1,
+            "formatted_date": {
+                "$dateToString": {
+                    "format": date_format,
+                    "date": {"$dateFromParts": date_group}
+                }
+            }
+        }},
+        {"$group": {
+            "_id": {"formatted_date": "$formatted_date"},
+            "totalValue": {"$sum": "$value"}
+        }},
+        {"$sort": {"_id.formatted_date": 1}},
+        {"$group": {
+            "_id": None,
+            "dataset": {"$push": "$totalValue"},
+            "labels": {"$push": "$_id.formatted_date"}
+        }},
+        {"$project": {
+            "_id": 0,
+            "dataset": 1,
+            "labels": 1
+        }}
+    ]
+
+    data = await collection.aggregate(query).to_list(None)
+    data = data[0]
+
+    pprint(data)
+
 
 if __name__ == '__main__':
-    asyncio.run(hello_mongo(db))
+    asyncio.run(aggregate_sum_from_date(db_=db,
+                                        dt_from="2022-09-01T00:00:00",
+                                        dt_upto="2022-12-31T23:59:00",
+                                        group_type="month"))
